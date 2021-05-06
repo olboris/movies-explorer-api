fc033e79109eb2a8de08c544cfd25d0368df32b6
Babushkina O <boriskinaos@mail.ru>
2021-05-06 14:31:08 +0300

    fix: исправить валидацию при регистрации

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required(),
-    name: Joi.string().required.min(2).max(30),
+    name: Joi.string().required().min(2).max(30),
   }),
 }), createUser);
 
